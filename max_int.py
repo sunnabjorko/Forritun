9850be3141006d60d207f8ad12f80093e95b03de
Sunna  Björk <sunnabjork@Sunnas-MacBook-Air.local>
2018-09-04 10:35:28 +0000

    Algorithm written for sequence

---
+++ max_int.py
@@ -0,0 +1,11 @@
+
+max_int = 0
+num_int = 0
+# Fill in the missing code
+
+while num_int >= 0:
+  num_int = int(input("Input a number: ")) 
+  
+  if num_int > max_int:
+      max_int = num_int
+print("The maximum is", max_int)  
